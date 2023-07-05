9a773bd71ce71a69d1b6668a180d289d0979098c
Warwick Stone <warwick_stone@fastmail.com>
2023-07-05 19:58:35 +1000

    WIP: ballcam

---
+++ Assets/Scripts/Ballcam.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Ballcam : MonoBehaviour
+{
+    public GameObject car;
+
+    private Vector3 _initialOffset;
+    private Quaternion _initialRotation;
+    private CustomCarControl _carControl;
+
+    private Vector3 camOffset;
+
+    void Start()
+    {
+        _initialOffset = transform.position;
+        _initialRotation = transform.rotation;
+        _carControl = GameObject.Find("CustomCar").GetComponent<CustomCarControl>();
+    }
+
+    void LateUpdate()
+    {
+        if (_carControl.IsOnGround)
+            camOffset = car.transform.TransformDirection(_initialOffset);
+
+        transform.position = car.transform.position + camOffset;
+
+        if (_carControl.IsOnGround)
+            transform.rotation = car.transform.rotation * _initialRotation;
+    }
+}
